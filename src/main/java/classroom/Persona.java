94315c5e3b3fc197a5075a4a4ebb441613002e53
Johan Ramirez <159493340+joramirezma@users.noreply.github.com>
2024-03-02 23:42:25 -0500

    Update Persona.java

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,13 +2,16 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    /*final*/ long cedula=0;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
-        cedula = 3;
+        //cedula = 3;
+    }
+
+    public Persona() {
     }
 
     public Persona(long cedula, String nombre) {
@@ -16,7 +19,7 @@ public class Persona {
         this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public Persona(String nombre, long cedula) {
         this.cedula = cedula;
         this.nombre = nombre;
@@ -31,9 +34,10 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        this.cedula=1;
         totalPersonas++;
     }
-    
+
     public long getCedula() {
         return cedula;
     }
